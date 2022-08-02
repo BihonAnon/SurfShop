3e0041d2fc7a0a3a3733a4ee3b6331b9f0a4e304
Alan Abed <alanabed@Alans-Air.webpass.net>
2022-08-02 14:23:17 -0700

    added User/Product/Order Models

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,17 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Product, Category } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = resolvers;
