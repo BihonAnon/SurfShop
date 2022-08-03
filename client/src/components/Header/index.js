a79745dc4edc59be614bd3753fd1adfa6839da67
amjr86-DBxjRd <amjr86@outlook.com>
2022-08-03 15:09:18 -0700

    pull

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 export default function Header() {
   return (
-      <div class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" style="font-family: Montserrat Subrayada; font-size: 80px;">Surf</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <div className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" style="font-family: Montserrat Subrayada; font-size: 80px;">Surf</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page">Home</a>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">Features</a>
+              <li className="nav-item">
+                <a className="nav-link">Features</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link">Pricing</a>
+              <li className="nav-item">
+                <a className="nav-link">Pricing</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
           </div>
