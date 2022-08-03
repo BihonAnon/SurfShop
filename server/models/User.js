df1281a5d103fb0d220e5b139d34a499527b6ac7
Alan Abed <alanabed@Alans-Air.webpass.net>
2022-08-03 11:13:45 -0700

    some resolver code

--- server/models/User.js
+++ server/models/User.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
 const Order = require('./Order');
+const Comment = require('./Comment');
 
 const userSchema = new Schema({
     username: {
