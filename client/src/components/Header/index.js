7a510dd39e06d281b2b251c30818f0b0be4c96e3
byron <byrondalberg@gmail.com>
2022-08-03 14:10:59 -0700

    ++Header(broke)

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <div>
-      HEADER
-      <p>a;ldjkf;asldfasdfklasd;flkalskdfj;asdf</p>
-      <p>a;ldjkf;asldfasdfklasd;flkalskdfj;asdf</p>
-      <p>a;ldjkf;asldfasdfklasd;flkalskdfj;asdf</p>
-      <p>a;ldjkf;asldfasdfklasd;flkalskdfj;asdf</p>
-      <p>a;ldjkf;asldfasdfklasd;flkalskdfj;asdf</p>
-      
-    </div>
+      <div class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" style="font-family: Montserrat Subrayada; font-size: 80px;">Surf</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
   );
 }
