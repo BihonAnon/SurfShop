2044d34ffa0e4769fb552cc3039bae12ad901bbf
Hail Choi <julianchoiii11@gmail.com>
2022-08-05 14:09:33 -0700

    Template

---
+++ client/src/components/StripeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Elements} from "@stripe/react-stripe.js"
+import {loadStripe} from "@stripeContainer.stripe-js"
+
+const PUBLIC_KEY = "pk_test_51LTXdACo4NaDfBdGThlBwZ7B8Ot1rmwl7OhJjJLWKmHHRJxfWGS5avV1Qc77HpwhwXxdEZsLnI6oQ4dUBzpLOl4c008yx32D7w"
+
+const stripeTestPromise = loadStripe(PUBLIC_KEY)
+export default function StripeContainer() {
+    return (
+        <Elements stripe={stripeTestPromise}>
+            <PaymentForm />
+        </Elements>
+    )
+}
